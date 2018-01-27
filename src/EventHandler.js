d5c9accdaf2f5f57e0d062f9e78b3001dccbff16
solo5star <solo_5@naver.com>
2018-01-27 19:36:19 +0900

    Add EventHandler

---
+++ src/EventHandler.js
@@ -0,0 +1,25 @@
+function EventHandler(listeners){
+	this.listeners = listeners;
+}
+EventHandler.prototype = {
+	identifierIds: [],
+
+	register: function(){
+		if(this.listeners === undefined){
+			throw "Attempted to register without listeners";
+		}
+		var events = EventManager.getAllEvents();
+
+		var listeners = this.listeners;
+
+		for(var event in this.listeners){
+			if(events.indexOf(event) != -1){
+				this.identifierIds.push(EventManager[event](args => listeners[event](args)));
+			}
+		}
+	},
+
+	unregister: function(){
+		this.identifierIds.forEach(id => EventManager.unregister(id));
+	}
+}
